66d8703620f92a09259cfe4858764241829ee469
Jitesh Soni <malvika.khiwal@gmail.com>
2022-11-24 11:48:24 -0500

    added snapshot

--- models/snapshot/daily_products_sold.sql
+++ models/snapshot/daily_products_sold.sql
@@ -5,18 +5,27 @@
     )
 }}
 
-
-
 with date_dim as (
     select *
     from {{ ref('date_dim') }}
     {% if is_incremental() %}
 
         -- this filter will only be applied on an incremental run
-    where cast(date_day as date) > (select max(snapshot_day) from {{ this }})
+        where cast(date_day as date) > (select max(snapshot_day) from {{ this }})
 
     {% endif %}
-),
+)
+,
+
+date_dim_product as (
+    select
+        cast(date_day as date) as snapshot_day,
+        product_id
+    from {{ ref('date_dim') }}
+    cross join {{ ref('dim_products') }}
+
+)
+,
 
 fct_sales as (
     select *
@@ -24,24 +33,25 @@ fct_sales as (
     {% if is_incremental() %}
 
         -- this filter will only be applied on an incremental run
-    where cast(order_purchase_timestamp as date) > (select max(snapshot_day) from {{ this }})
+        where cast(order_purchase_timestamp as date) > (select max(snapshot_day) from {{ this }})
 
     {% endif %}
 )
 
 select
-    cast(date_day as date) as snapshot_day,
-    product_id,
+    snapshot_day,
+    date_dim_product.product_id,
     count(order_id) as total_units_sold
 from
-    date_dim
+    date_dim_product
 left join
     fct_sales
     on
-        cast(date_day as date) = cast(order_purchase_timestamp as date)
+        snapshot_day = cast(order_purchase_timestamp as date)
+    and date_dim_product.product_id = fct_sales.product_id
 where
     -- Filter the date dimension to give it a relevant time window
-    date_day >= (
+    snapshot_day >= (
         select min(cast(order_purchase_timestamp as date))
         from
             fct_sales)
